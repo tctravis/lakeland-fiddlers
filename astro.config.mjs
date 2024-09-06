f28b8ba997fa884fb49921a922c446475a88007d
toby-morph <105436732+toby-morph@users.noreply.github.com>
2024-09-06 12:37:19 +0100

    Adding tunes index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
-import image from "@astrojs/image";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind(), image()]
+  integrations: [vue(), tailwind()]
 });
