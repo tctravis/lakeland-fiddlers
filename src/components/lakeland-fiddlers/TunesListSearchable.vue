e5b113b093abb4fe885adc3afd48e3933e6e4970
toby-morph <105436732+toby-morph@users.noreply.github.com>
2025-04-01 11:30:04 +0100

    Adding vue filter list

--- src/components/lakeland-fiddlers/TunesListSearchable.vue
+++ src/components/lakeland-fiddlers/TunesListSearchable.vue
@@ -1,17 +1,21 @@
 <template>
-    <div>
-        <h1>Tune List</h1>
-        <p>List of tunes for the Lakeland Fiddlers.</p>
-        <ul>
-            <li v-for="tune in tunes" :key="tune.id">
-                {{ tune.name }}
+    <div class="space-y-4">
+       <div class="flex flex-wrap gap-2">
+          <label for="tuneTitle" class="w-full sm:w-auto">Filter by tune title: </label>
+          <input class="w-full sm:w-auto" name="tuneTitle" type="text" v-model="titleFilter" placeholder="Search for a tune..." />
+          <button @click="titleFilter = ''" class="bg-red-500 text-white px-2 py-1 rounded">Clear</button>
+        </div>
+        
+        <ul class="space-y-2">
+            <li v-for="tune in filteredTunes" :key="tune.id">
+                <a class="underline" :href="`/tunes/${tune.id}/`">{{ tune.data.title }}</a>
             </li>
         </ul>
     </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'; 
+import { ref, computed } from 'vue'; 
 
 const props = defineProps({
   tunes: {
@@ -19,6 +23,22 @@ const props = defineProps({
     required: true
   }
 })
+
+const tunes = ref([...props.tunes]);
+
+const titleFilter = defineModel('titleFilter', {
+  type: String,
+  default: ''
+});
+
+const filteredTunes = computed(() => {
+      if (titleFilter.value === '') {
+        return tunes.value;
+      }
+      return tunes.value.filter((tune) => {
+        return tune.data.title.toLowerCase().includes(titleFilter.value.toLowerCase())
+      });
+    });
 </script>
 
 <style lang="scss" scoped>
