c0fd7fac9f642c43ccc6727286e337442674fdbc
toby-morph <105436732+toby-morph@users.noreply.github.com>
2025-04-01 10:03:58 +0100

    Changing slug to id

---
+++ src/components/lakeland-fiddlers/TunesListSearchable.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Tune List</h1>
+        <p>List of tunes for the Lakeland Fiddlers.</p>
+        <ul>
+            <li v-for="tune in tunes" :key="tune.id">
+                {{ tune.name }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'; 
+
+const props = defineProps({
+  tunes: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
